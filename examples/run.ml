de7232d5c404ca7158fa0c29d87de7fab21d785d
Joel Bjornson <joel.bjornson@gmail.com>
2021-05-07 10:51:01 +0100

    Rever examples

--- examples/run.ml
+++ examples/run.ml
@@ -1,33 +1,13 @@
-(* open Popper
-
-   let suite = suite [ ("Hello World", Hello_world.suite) ; ("Expression",
-   Exp.suite) ; ("Chart", Chart.suite) ; ("Arithmetic", Arithmetic.suite) ;
-   ("Tree", Tree.suite) ; ("Equal", Equal.suite) ]
-
-   let () = run suite *)
-
 open Popper
-open Sample.Syntax
 
-type contact =
-  | Email of string
-  | Mail of
-      { street : string
-      ; number : int option
-      ; zip : string
-      }
-[@@deriving show, ord, popper]
+let suite =
+  suite
+    [ ("Hello World", Hello_world.suite)
+    ; ("Expression", Exp.suite)
+    ; ("Chart", Chart.suite)
+    ; ("Arithmetic", Arithmetic.suite)
+    ; ("Tree", Tree.suite)
+    ; ("Equal", Equal.suite)
+    ]
 
-let test () =
-  let* contact, consumed =
-    sample_contact
-    |> Sample.resize 10
-    |> Sample.with_log "contact" pp_contact
-    |> Sample.with_consumed
-  in
-  Format.printf "%a@.%a@." Consumed.pp consumed pp_contact contact;
-  match contact with
-  | Email _ -> pass
-  | Mail _ -> fail "Expected email"
-
-let () = check test
+let () = run suite
