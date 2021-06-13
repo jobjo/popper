483bebbb8a3caca85a555aae3cf26eda56471e55
Bikal Lem <gbikal@gmail.com>
2021-06-13 17:59:04 +0100

    add int32, int64, char and bytes to comparator

--- src/lib/comparator.mli
+++ src/lib/comparator.mli
@@ -4,9 +4,13 @@ val make : ('a -> 'a -> int) -> (Format.formatter -> 'a -> unit) -> 'a t
 val compare : 'a t -> 'a -> 'a -> int
 val pp : 'a t -> Format.formatter -> 'a -> unit
 val int : int t
+val int32 : int32 t
+val int64 : int64 t
 val float : float t
 val bool : bool t
+val char : char t
 val string : string t
+val bytes : bytes t
 val tuple : 'a t -> 'b t -> ('a * 'b) t
 val list : 'a t -> 'a list t
 val array : 'a t -> 'a array t
